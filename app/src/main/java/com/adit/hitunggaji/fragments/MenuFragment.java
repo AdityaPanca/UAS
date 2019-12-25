91eb0701466ab42cbaf8f66f2fef02ecedf2e25c
AdityaPanca <aditya.annecdo@gmail.com>
2019-12-25 14:01:24 +0700

    Membuat MenuFragment

---
+++ app/src/main/java/com/adit/hitunggaji/fragments/MenuFragment.java
@@ -0,0 +1,82 @@
+package com.adit.hitunggaji.fragments;
+
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.adit.hitunggaji.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MenuFragment extends Fragment {
+
+
+    private OnFragmentInteractionListener mListener;
+
+    public MenuFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_menu, container, false);
+        ImageView GajiButton = view.findViewById(R.id.imageView);
+
+        GajiButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mListener != null){
+                    mListener.onFragmentGajiKaryawanCliked();
+                }
+            }
+        });
+
+
+        return view;
+
+
+
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        void onFragmentGajiKaryawanCliked();
+    }
+}
